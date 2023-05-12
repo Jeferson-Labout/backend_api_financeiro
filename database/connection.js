61d11dded12a1f12dc9d8469f554e753d35289eb
Jeferson Labout <jefson1989@gmail.com>
2023-05-12 17:29:27 -0300

    mongo

--- database/connection.js
+++ database/connection.js
@@ -5,7 +5,8 @@ const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_PASS;
 
 const connect = () => {
-    mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.0m8dtgl.mongodb.net/?retryWrites=true&w=majority`)
+    // mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.0m8dtgl.mongodb.net/?retryWrites=true&w=majority`)
+    mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.fitfv17.mongodb.net/moneyWise?retryWrites=true&w=majority`)
 
     const connection = mongoose.connection;
 
